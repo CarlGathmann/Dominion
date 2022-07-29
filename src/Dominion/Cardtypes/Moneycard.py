9e30506658110579cc4dfa484eeca99637d8164f
cgathmann <c.gathmann@icloud.com>
2022-07-29 18:14:12 +0200

    went back to working state

--- src/Dominion/Cardtypes/Moneycard.py
+++
@@ -1,9 +0,0 @@
-from src.Dominion.Card import Card
-
-
-class Moneycard(Card):
-    pass
-
-    def __init__(self, expenses, money):
-        super().__init__(expenses)
-        self.money = money
--- src/Dominion/Cardtypes/VictoryCard.py
+++ src/Dominion/Cardtypes/VictoryCard.py
@@ -2,7 +2,6 @@ from src.Dominion.Card import Card
 
 
 class VictoryCard(Card):
-
     def __init__(self, expenses, victory_points):
         super().__init__(expenses)
         self.victory_points = victory_points
